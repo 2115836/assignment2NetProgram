89fdacc92e6b7db67a501dc915d01135117524e4
2115836 <151827748+2115836@users.noreply.github.com>
2024-01-15 15:47:45 +0000

    Add files via upload

--- Assignment2NetProgram.py
+++ Assignment2NetProgram.py
@@ -13,7 +13,8 @@ def get_weather(api_key, city):
 
         if response.status_code == 200:
             print(f"Weather in {city}:")
-        
+            print(f"Temperature: {data['current']['temperature']}°C")
+            
         else:
             print(f"Error: {data['error']['info']}")
 
