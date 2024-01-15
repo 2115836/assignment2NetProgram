3142291e6e11ea3c0a36393617552d91cd3c860f
2115836 <151827748+2115836@users.noreply.github.com>
2024-01-15 15:27:33 +0000

    Add files via upload

--- Assignment2NetProgram.py
+++ Assignment2NetProgram.py
@@ -1,2 +1,8 @@
 import requests
 
+def get_weather(api_key, city):
+    base_url = "http://api.weatherstack.com/current"
+    params = {
+        "access_key": api_key,
+        "query": city
+    }
